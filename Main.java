9df405e7bfdc5bb4220e7bed1f57789d96ae7e81
ritik ramuka <ritik.ramuka@gmail.com>
2022-09-14 22:43:59 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // char[] str1 = {'R', 'I', 'T', 'I', 'K'};
+        // System.out.println(str1);
+
+        // System.out.println("----------------");
+
+        // datatype
+        // String str = "RITIK";
+        // System.out.println(str);
+
+        // class behaviour
+        // String str2 = new String("Ritik");
+        // System.out.println(str2);
+
+        // String from char array
+        // char[] str1 = {'R', 'I', 'T', 'I', 'K'};
+        // String str3 = new String(str1);
+        // System.out.println(str3);
+
+        // Immutable Strings
+
+        // arrays mutable
+        // int[] arr = { 1, 2, 3 };
+        // for (int i = 0; i < 3; i++) {
+        // System.out.print(arr[i]);
+        // }
+        // System.out.println();
+        // arr[0] = 9;
+        // for (int i = 0; i < 3; i++) {
+        // System.out.print(arr[i]);
+        // }
+
+        // String str = "accio";
+        // str = str.replace('a', 'b');
+        // System.out.println(str);
+
+        // String str1 = "accio";
+        // String str2 = "accio";
+        // str1 = str1.replace('a', 'b');
+        // System.out.println(str1);
+        // System.out.println(str2);
+
+        // ==
+        // String str1 = "accio";
+        // String str2 = "accio";
+        // System.out.println(str1.equals(str2));
+        // String str3 = new String("accio");
+        // String str4 = new String("accio");
+        // System.out.println(str3.equals(str4));
+
+        // methods
+        // String str1 = "Ritik";
+        // System.out.println(str1.charAt(4));
+
+        // System.out.println(str1.length());
+
+        // String str = "acciojobs";
+        // System.out.println(str.substring(3));
+        // System.out.println(str.substring(3, 5));
+        // System.out.println(str.charAt(5));
+
+        // String str2 = "abcbdbf";
+        // String[] arr = str2.split("b");
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+
+        // uppercase, lowercase
+        // String str = "riTik";
+        // System.out.println(str);
+        // "RITIK"
+        // System.out.println(str.toUpperCase());
+
+        // String str = "RiTiK";
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str);
+    }
+}
